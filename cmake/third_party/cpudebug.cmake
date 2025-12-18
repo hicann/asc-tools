a90adc1af1e3bfde92eeb331212f1c5a4c42e24f
cann-robot <songchangxia@cann.team>
2025-12-18 21:50:08 +0800

    adapt to new library name
    Co-authored-by: 王旭<wangxu243@huawei.com>

--- cmake/third_party/cpudebug.cmake
+++ cmake/third_party/cpudebug.cmake
@@ -10,11 +10,21 @@
 
 set(PKG_NAME "cpudebug_deps")
 
+if (CMAKE_SYSTEM_PROCESSOR MATCHES "x86_64")
+    message(STATUS "Detected architecture: x86_64")
+    set(TAR_ARCH x86)
+elseif (CMAKE_SYSTEM_PROCESSOR MATCHES "aarch64|arm64|arm")
+    message(STATUS "Detected architecture: ARM64")
+    set(TAR_ARCH aarch64)
+else ()
+    message(WARNING "Unknown architecture: ${CMAKE_SYSTEM_PROCESSOR}")
+endif ()
+
 if (NOT EXISTS ${CMAKE_SOURCE_DIR}/libraries/lib/include/stub_fun.h)
-  set(CPUDEBUG_PKG_NAME cpudebug_deps_linux-${CMAKE_SYSTEM_PROCESSOR}.tar.gz)
+  set(CPUDEBUG_PKG_NAME cann-asc-tools-cpudebug-deps-lib_8.5.0_linux-${CMAKE_SYSTEM_PROCESSOR}.tar.gz)
   set(CPUDEBUG_PKG_PATH ${DEPS_FILE_PATH}/${CPUDEBUG_PKG_NAME})
   if (NOT EXISTS ${CPUDEBUG_PKG_PATH})
-      set(CPUDEBUG_PKG_URL "https://mirrors.huaweicloud.com/artifactory/cann-run/8.5.0.alpha001/try/${CMAKE_SYSTEM_PROCESSOR}/${CPUDEBUG_PKG_NAME}")
+      set(CPUDEBUG_PKG_URL "https://container-obsfs-filesystem.obs.cn-north-4.myhuaweicloud.com/package/cann/asc-tools-dev/version_compile/master/202512/20251218/ubuntu_${TAR_ARCH}/${CPUDEBUG_PKG_NAME}")
       message(STATUS "${PKG_NAME} pkg not found in ${DEPS_FILE_PATH}, downloading from ${CPUDEBUG_PKG_URL}")
   else()
       set(CPUDEBUG_PKG_URL ${CPUDEBUG_PKG_PATH})
@@ -31,7 +41,7 @@ if (NOT EXISTS ${CMAKE_SOURCE_DIR}/libraries/lib/include/stub_fun.h)
   FetchContent_MakeAvailable(${PKG_NAME})
 
   execute_process(
-    COMMAND tar -xf ${CPUDEBUG_PKG_PATH} -C ${CMAKE_SOURCE_DIR}/libraries --strip-components 3
+    COMMAND tar -xf ${CPUDEBUG_PKG_PATH} -C ${CMAKE_SOURCE_DIR}/libraries --strip-components 1
     WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}/libraries
     RESULT_VARIABLE result
   )
