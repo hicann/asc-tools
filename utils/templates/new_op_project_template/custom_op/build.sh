8f501a228f7c8556cf34db07f3948243ae6d4561
zhanglong99 <zhanglong170@huawei.com>
2026-03-28 16:55:07 +0800

    fix msogpen build.sh template
    Co-authored-by: zhanglong99<zhanglong170@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !210 merge 9.0.0 into 9.0.0
    
    fix msogpen build.sh template
    
    Created-by: zhanglong99
    Commit-by: zhanglong99
    Merged-by: cann-robot
    Description: ## 描述
    修改msopgen新生成模板内容
    
    ## 关联的Issue
    <!-- 如果这个PR是为了解决特定的Issue，请在这里提供Issue链接。例如：关联Issue #123-->
    <!-- 如果这个PR是为了解决特定的问题单，请在这里描述问题单单号。-->
    
    ## 测试
    <!--描述进行了哪些测试来验证你的改动。包括但不限于二级冒烟、算子泛化等。-->
    
    ## 文档更新
    <!--如果这个PR包含文档的更新，请在这里指出。例如：更新了README.md文件。-->
    
    ## 类型标签
    <!--  [x] 表示选中 -->
    - [x] 🐛 fix: Bug 修复
    - [ ] ✨ feat: 新功能
    - [ ] ⚡ perf: 性能优化
    - [ ] ♻️ refactor: 代码重构
    - [ ] 🧪 test: 新增或修改测试
    - [ ] 📝 docs: 文档更新
    - [ ] 🔧 ci: CI/CD 配置修改
    - [ ] ↩️ revert: 回退
    - [ ] 🧹 chore: 其他，请具体描述
    
    
    See merge request: cann/asc-tools!210

--- utils/templates/new_op_project_template/custom_op/build.sh
+++ utils/templates/new_op_project_template/custom_op/build.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 # ----------------------------------------------------------------------------------------------------------
 # Copyright (c) 2025 Huawei Technologies Co., Ltd.
 # This program is free software, you can redistribute it and/or modify it under the terms and conditions of
@@ -8,7 +9,6 @@
 # See LICENSE in the root of the software repository for the full text of the License.
 # ----------------------------------------------------------------------------------------------------------
 
-#!/bin/bash
 if [ -z "$BASE_LIBS_PATH" ]; then
     if [ -z "$ASCEND_HOME_PATH" ]; then
         if [ -z "$ASCEND_AICPU_PATH" ]; then
@@ -27,24 +27,14 @@ echo "using ASCEND_HOME_PATH: $ASCEND_HOME_PATH"
 script_path=$(realpath $(dirname $0))
 
 BUILD_DIR="build_out"
-HOST_NATIVE_DIR="host_native_tiling"
 mkdir -p build_out
 rm -rf build_out/*
 opts=$(python3 $ASCEND_HOME_PATH/tools/tikcpp/ascendc_kernel_cmake/fwk_modules/util/preset_parse.py $script_path/CMakePresets.json)
-
-ENABLE_CROSS="-DENABLE_CROSS_COMPILE=True"
-ENABLE_BINARY="-DENABLE_BINARY_PACKAGE=True"
-ENABLE_LIBRARY="-DASCEND_PACK_SHARED_LIBRARY=True"
 cmake_version=$(cmake --version | grep "cmake version" | awk '{print $3}')
-
-target=package
-if [ "$1"x != ""x ]; then target=$1; fi
 
 if [ "$cmake_version" \< "3.19.0" ] ; then
     cmake -S . -B "$BUILD_DIR" $opts
 else
     cmake -S . -B "$BUILD_DIR" --preset=default
 fi
-cmake -S . -B "$BUILD_DIR" --preset=default
-cmake --build "$BUILD_DIR" --target binary -j$(nproc)
-cmake --build "$BUILD_DIR" --target $target -j$(nproc)
+cmake --build "$BUILD_DIR" --target binary package -j$(nproc)
