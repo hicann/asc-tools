78adfe4372a19dd13357007040bbf98c2a7907e2
chupeng1996 <chupeng5@huawei.com>
2026-04-15 10:19:33 +0800

    修复bash build.sh --pkg 之后，执行 bash build.sh -t 失败问题
    Co-authored-by: chupeng1996<chupeng5@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !250 merge fix_ut into master
    
    修复bash build.sh --pkg 之后，执行 bash build.sh -t 失败问题
    
    Created-by: chupeng1996
    Commit-by: chupeng1996
    Merged-by: cann-robot
    Description: ## 描述
    <!--在这里详细描述你的改动，包括改动的原因和所采取的方法。-->
    修复bash build.sh --pkg 之后，执行 bash build.sh -t 失败问题
    ## 关联的Issue
    <!-- 如果这个PR是为了解决特定的Issue，请在这里提供Issue链接。例如：关联Issue #123-->
    <!-- 如果这个PR是为了解决特定的问题单，请在这里描述问题单单号。-->
    NA
    ## 测试
    <!--描述进行了哪些测试来验证你的改动。包括但不限于二级冒烟、算子泛化等。-->
    bash build.sh --pkg 之后，执行 bash build.sh -t 不失败
    ## 文档更新
    <!--如果这个PR包含文档的更新，请在这里指出。例如：更新了README.md文件。-->
    NA
    ## 类型标签
    <!--  [x] 表示选中 -->
    - [x] 🐛 fix: Bug 修复
    - [ ] ✨ feat: 新功能
    - [ ] ⚡ perf: 性能优化
    - [ ] ♻️ refactor: 代码重构
    - [ ] 🧪 test: 新增或修改测试
    - [ ] 📝 docs: 文档更新
    - [ ] 🔧 ci: CI/CD 配置修改
    - [ ] ↩️ revert: 回退
    - [ ] 🧹 chore: 其他，请具体描述
    
    
    See merge request: cann/asc-tools!250

--- cmake/third_party/cpudebug.cmake
+++ cmake/third_party/cpudebug.cmake
@@ -29,12 +29,12 @@ if (IS_DIRECTORY ${CMAKE_SOURCE_DIR}/libraries/lib AND NOT EXISTS ${CMAKE_SOURCE
 endif()
 
 if (NOT EXISTS ${CMAKE_SOURCE_DIR}/libraries/lib/include/stub_fun.h)
+    set(CPUDEBUG_PKG_NAME cann-asc-tools-cpudebug-deps-lib_${BUILD_TYPE_LOWER}_9.0.0_linux-${CMAKE_SYSTEM_PROCESSOR}.tar.gz)
     file(GLOB CPUDEBUG_PKG
         LIST_DIRECTORIES True
-        ${CANN_3RD_LIB_PATH}/cann-asc-tools-cpudebug-deps*.tar.gz
+        ${CANN_3RD_LIB_PATH}/${CPUDEBUG_PKG_NAME}
     )
 
-    set(CPUDEBUG_PKG_NAME cann-asc-tools-cpudebug-deps-lib_${BUILD_TYPE_LOWER}_9.0.0_linux-${CMAKE_SYSTEM_PROCESSOR}.tar.gz)
     if(NOT EXISTS ${CPUDEBUG_PKG})
         set(CPUDEBUG_PKG_URL "https://container-obsfs-filesystem.obs.cn-north-4.myhuaweicloud.com/package/cann/asc-toolkit-dev/version_compile/master/202603/20260325/ubuntu_${TAR_ARCH}/${CPUDEBUG_PKG_NAME}")
         message(STATUS "cpudebug pkg not found in ${CANN_3RD_LIB_PATH}, downloading from ${CPUDEBUG_PKG_URL}")
