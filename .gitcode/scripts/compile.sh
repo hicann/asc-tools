1916bb4281056f3f6352c7b3c4d98a8e917a3f3c
yajie_caroline <chenyajie6@huawei.com>
2026-07-27 19:41:44 +0800

    增加代码化workflow
    Co-authored-by: chenyajie<chenyajie6@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !389 merge master into master
    
    增加代码化workflow
    
    Created-by: yajie_caroline
    Commit-by: chenyajie
    Merged-by: cann-robot
    Description: ## 描述
    <!--在这里详细描述你的改动，包括改动的原因和所采取的方法。-->
    
    ## 关联的Issue
    <!-- 如果这个PR是为了解决特定的Issue，请在这里提供Issue链接。例如：关联Issue #123-->
    <!-- 如果这个PR是为了解决特定的问题单，请在这里描述问题单单号。-->
    
    ## 测试
    <!--描述进行了哪些测试来验证你的改动。包括但不限于二级冒烟、算子泛化等。-->
    
    ## 文档更新
    <!--如果这个PR包含文档的更新，请在这里指出。例如：更新了README.md文件。-->
    
    ## 类型标签
    <!--  [x] 表示选中 -->
    - [ ] 🐛 fix: Bug 修复
    - [ ] ✨ feat: 新功能
    - [ ] ⚡ perf: 性能优化
    - [ ] ♻️ refactor: 代码重构
    - [ ] 🧪 test: 新增或修改测试
    - [ ] 📝 docs: 文档更新
    - [ ] 🔧 ci: CI/CD 配置修改
    - [ ] ↩️ revert: 回退
    - [ ] 🧹 chore: 其他，请具体描述
    
    
    See merge request: cann/asc-tools!389

---
+++ .gitcode/scripts/compile.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+# -----------------------------------------------------------------------------------------------------------
+# Copyright (c) 2026 Huawei Technologies Co., Ltd.
+# This program is free software, you can redistribute it and/or modify it under the terms and conditions of
+# CANN Open Software License Agreement Version 2.0 (the "License").
+# Please refer to the License for details. You may not use this file except in compliance with the License.
+# THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+# INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+# See LICENSE in the root of the software repository for the full text of the License.
+# -----------------------------------------------------------------------------------------------------------
+set -e
+set -o pipefail
+cd ${WORKSPACE}
+echo $(grep -E "^VERSION_ID=" /etc/os-release | cut -d'"' -f2)
+if [[ "${task_name}" =~ x86_compile_ubuntu24 ]]; then
+    echo "api-check=compile" >> "${ATOMGIT_OUTPUT}"
+else
+    echo "api-check=continue" >> "${ATOMGIT_OUTPUT}"
+fi
+if [[ "${task_name}" == *ubuntu24* || "${task_name}" == *24* ]]; then
+    sudo update-alternatives --set gcc /usr/bin/gcc-14
+else
+    if [[ -f "/opt/rh/devtoolset-7/enable" ]]; then
+        echo "source devtoolset"
+        source /opt/rh/devtoolset-7/enable
+    fi
+fi
+gcc --version
+source /home/jenkins/Ascend/cann/bin/setenv.bash
+case "${task_name}" in
+    x86_compile_ubuntu24)
+        sed -i "1i set(CMAKE_EXPORT_COMPILE_COMMANDS ON)" "CMakeLists.txt"
+        bash build.sh --cann_3rd_lib_path=/home/jenkins/opensource
+        echo "exec cmd: [bash build.sh --cann_3rd_lib_path=/home/jenkins/opensource]"
+        ;;
+    x86_compile)
+        bash build.sh --cann_3rd_lib_path=/home/jenkins/opensource
+        echo "exec cmd: [bash build.sh --cann_3rd_lib_path=/home/jenkins/opensource]"
+        exit 0
+        ;;
+    arm_compile)
+        bash build.sh --cann_3rd_lib_path=/home/jenkins/opensource
+        echo "exec cmd: [bash build.sh --cann_3rd_lib_path=/home/jenkins/opensource]"
+        exit 0
+        ;;
+esac
---
+++ .gitcode/scripts/pre_smoke.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+# -----------------------------------------------------------------------------------------------------------
+# Copyright (c) 2026 Huawei Technologies Co., Ltd.
+# This program is free software, you can redistribute it and/or modify it under the terms and conditions of
+# CANN Open Software License Agreement Version 2.0 (the "License").
+# Please refer to the License for details. You may not use this file except in compliance with the License.
+# THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+# INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+# See LICENSE in the root of the software repository for the full text of the License.
+# -----------------------------------------------------------------------------------------------------------
+set +e
+
+WORKSPACE=/home/taskspace
+cd /home/taskspace
+
+log() {
+  echo "==== $(date '+%Y%m%d.%H%M%S') : $* ===="
+}
+
+rm -rf /root/ascend/log
+
+export ASCEND_GLOBAL_LOG_LEVEL=2
+export ASCEND_SLOG_PRINT_TO_STDOUT=0
+
+log "start run test case"
+
+# 下载并安装 asc-tools 包
+arm_package="cann-asc-tools_linux-aarch64.run"
+wget -nv -O "${arm_package}" "https://ascend-ci.obs.cn-north-4.myhuaweicloud.com/${obs_path}/${arm_package}" 2>/dev/null
+if [ ! -f "${arm_package}" ] || [ ! -s "${arm_package}" ]; then
+  echo "No custom package found, This PR no need execute smoke."
+  rm -f "${arm_package}"
+  exit 0
+fi
+
+chmod +x "${arm_package}"
+source /usr/local/Ascend/cann/set_env.sh
+yes "y" | bash "${arm_package}" --full --install-path=/usr/local/Ascend --quiet
+python3 -m pip install tensorflow -i https://pypi.tuna.tsinghua.edu.cn/simple
+source /usr/local/Ascend/cann/set_env.sh
+cd ./scripts && bash run_presmoke.sh 2>&1 | tee -a ./run_test.log
+
+# 打包plog
+mkdir -p /root/ascend
+slog_name="slog.tar.gz"
+tar -zcf slog.tar.gz -C /root/ascend log
+OBS_KEY="${obs_smoke_path}/plog/${slog_name}"
+# upload plog
+if python3 /home/upload.py --bucket-name "ascend-ci" --action upload  --local-file "slog.tar.gz" --obs-object-key "${OBS_KEY}"; then
+  echo "::set-output var=plog_url:https://ascend-ci.obs.cn-north-4.myhuaweicloud.com/${OBS_KEY}"
+fi
+
+# 检查结果
+log "checking test results"
+date_time=$(date +%Y%m%d.%H%M%S)
+if grep -w -e "execute samples success" ./run_test.log; then
+  echo "$date_time : run test case success"
+else
+  echo "$date_time : run test case failed"
+  exit 1
+fi
