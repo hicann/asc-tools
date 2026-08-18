c9ce92deac3251e2e1428c0744d46c1af63b8a1f
yajie_caroline <chenyajie6@huawei.com>
2026-08-18 14:13:23 +0800

    上线cce
    Co-authored-by: chenyajie<chenyajie6@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !429 merge master into master
    
    上线cce
    
    Created-by: yajie_caroline
    Commit-by: chenyajie
    Merged-by: cann-robot
    Description: ## 描述
    <!--在这里详细描述你的改动，包括改动的原因和所采取的方法。-->
    
    ## 关联的Issue
    <!-- 如果这个PR是为了解决特定的Issue，请在这里提供Issue链接。例如：关联Issue #123-->
    <!-- 如果这个PR是为了解决特定的问题单，请在这里描述问题单单号。-->
    
    ## 测试
    <!--描述进行了哪些测试来验证你的改动。包括但不限于二级冒烟、算子泛化等。-->
    
    ## 文档更新
    <!--如果这个PR包含文档的更新，请在这里指出。例如：更新了README.md文件。-->
    
    ## 类型标签
    <!--  [x] 表示选中 -->
    - [ ] 🐛 fix: Bug 修复
    - [ ] ✨ feat: 新功能
    - [ ] ⚡ perf: 性能优化
    - [ ] ♻️ refactor: 代码重构
    - [ ] 🧪 test: 新增或修改测试
    - [ ] 📝 docs: 文档更新
    - [ ] 🔧 ci: CI/CD 配置修改
    - [ ] ↩️ revert: 回退
    - [ ] 🧹 chore: 其他，请具体描述
    
    
    See merge request: cann/asc-tools!429

--- .gitcode/scripts/pre_smoke.sh
+++ .gitcode/scripts/pre_smoke.sh
@@ -10,8 +10,6 @@
 # -----------------------------------------------------------------------------------------------------------
 set +e
 
-WORKSPACE=/home/taskspace
-cd /home/taskspace
 
 log() {
   echo "==== $(date '+%Y%m%d.%H%M%S') : $* ===="
@@ -38,17 +36,13 @@ source /usr/local/Ascend/cann/set_env.sh
 yes "y" | bash "${arm_package}" --full --install-path=/usr/local/Ascend --quiet
 python3 -m pip install tensorflow -i https://pypi.tuna.tsinghua.edu.cn/simple
 source /usr/local/Ascend/cann/set_env.sh
-cd ./scripts && bash run_presmoke.sh 2>&1 | tee -a ./run_test.log
+bash ./scripts/run_presmoke.sh 2>&1 | tee -a ./run_test.log
 
 # 打包plog
 mkdir -p /root/ascend
 slog_name="slog.tar.gz"
 tar -zcf slog.tar.gz -C /root/ascend log
-OBS_KEY="${obs_smoke_path}/plog/${slog_name}"
-# upload plog
-if python3 /home/upload.py --bucket-name "ascend-ci" --action upload  --local-file "slog.tar.gz" --obs-object-key "${OBS_KEY}"; then
-  echo "::set-output var=plog_url:https://ascend-ci.obs.cn-north-4.myhuaweicloud.com/${OBS_KEY}"
-fi
+
 
 # 检查结果
 log "checking test results"
